6e606911d7cafd1e7612dbb1bee9d7c0d15eb287
NataliaDave <nataliadave@gmail.com>
2025-09-16 16:47:19 -0300

    clase Producto y ProductoManager

---
+++ src/grupo10_tp6/Producto.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo10_tp6;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto {
+    
+    private int codigo;
+    private String nombre;
+    private String rubro;
+    private double precio;
+
+    public Producto(int codigo, String nombre, String rubro, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.rubro = rubro;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+    
+}
---
+++ src/grupo10_tp6/ProductoManager.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo10_tp6;
+import java.util.*;
+/**
+ *
+ * @author Usuario
+ */
+public class ProductoManager {
+    private List <Producto> productos;
+    
+    public ProductoManager (){
+    
+            productos = new ArrayList<>();
+    
+    }
+    public Producto buscarPorCodigo(int codigo){
+    
+        for (Producto p : productos){
+        
+            if (p.getCodigo()==codigo){
+                return p;
+            }
+        
+        }
+        return null;
+     }
+    
+    public boolean agregar (Producto p){
+    
+        if (buscarPorCodigo(p.getCodigo())!=null){
+        
+            return false;
+        }
+        productos.add(p);
+        return true;
+    }
+    
+    public boolean eliminar (int codigo){
+        Producto p = buscarPorCodigo(codigo);
+        if(p !=null){
+            productos.remove(p);
+            return true;
+       }return false;
+   }
+    public boolean actualizar (Producto nuevo){
+        for(int i= 0; i<productos.size(); i++) {
+            if (productos.get(i).getCodigo()==nuevo.getCodigo()){
+                productos.set(i, nuevo);
+                return true;
+            }
+        
+        }
+        return false;
+    }
+    
+    public List <Producto> buscarProductoPorRubro(String rubro){
+        List<Producto> resultado = new ArrayList<>();
+        for(Producto p : productos){
+            if (p.getRubro().equalsIgnoreCase(rubro)) {
+            resultado.add(p);
+            }
+        }
+        return resultado;
+    
+    }
+    
+    public List <Producto> buscarPorPrecio(double min, double max){
+    
+        List<Producto> resultado = new ArrayList<>();
+        for(Producto p : productos) {
+            if(p.getPrecio()>=min && p.getPrecio()<=max){
+                resultado.add(p);
+            }
+        }
+        return resultado;
+    }
+    
+    public List <Producto> obtenerTodos(){
+        return new ArrayList<>(productos);
+    
+    }
+    
+}
